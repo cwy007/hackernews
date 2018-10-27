7ab9a91846b9df3fd975ef358ad6634557892e44
cwy007 <chanweiyan007@gmail.com>
2018-10-27 22:14:08 +0800

    Use ES6 constructor

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: list,
+    };
+  }
+
   render() {
-    const helloWorld = 'Welcome to the Road to learn React';
-
-    const list = [
-      {
-        title: 'React',
-        url: 'https://facebook.github.io/react/',
-        author: 'Jordan Walke',
-        num_comments: 3,
-        points: 4,
-        objectID: 0,
-      },
-      {
-        title: 'Redux',
-        url: 'https://github.com/reactjs/redux',
-        author: 'Dan Abramov, Andrew Clark',
-        num_comments: 2,
-        points: 5,
-        objectID: 1,
-      },
-    ];
-
     return (
       <div className="App">
-        {list.map(function(item) {
+        {this.state.list.map(function(item) {
           return (
             <div key={item.objectID}>
               <span>
