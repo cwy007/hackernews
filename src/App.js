ecdb8900699efc1f25aca2797dceca2925ff2339
cwy007 <chanweiyan007@gmail.com>
2018-11-07 21:55:53 +0800

    User ref property in functional component

--- src/App.js
+++ src/App.js
@@ -149,53 +149,58 @@ class App extends Component {
   }
 }
 
-class Search extends Component {
-  componentDidMount() {
-    if (this.input) {
-      this.input.focus();
-    }
-  }
+// class Search extends Component {
+//   componentDidMount() {
+//     if (this.input) {
+//       this.input.focus();
+//     }
+//   }
+//
+//   render() {
+//     const {
+//       value,
+//       onChange,
+//       onSubmit,
+//       children
+//     } = this.props;
+//
+//     return (
+//       <form onSubmit={onSubmit}>
+//         <input
+//           type="text"
+//           value={value}
+//           onChange={onChange}
+//           ref={(node) => { this.input = node; }}
+//         />
+//         <button type="submit">
+//           {children}
+//         </button>
+//       </form>
+//     );
+//   }
+// }
 
-  render() {
-    const {
-      value,
-      onChange,
-      onSubmit,
-      children
-    } = this.props;
-
-    return (
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          value={value}
-          onChange={onChange}
-          ref={(node) => { this.input = node; }}
-        />
-        <button type="submit">
-          {children}
-        </button>
-      </form>
-    );
-  }
+const Search = ({
+  value,
+  onChange,
+  onSubmit,
+  children
+}) => {
+  let input;
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        value={value}
+        onChange={onChange}
+        ref={(node) => input = node}
+      />
+      <button type="submit">
+        {children}
+      </button>
+    </form>
+  );
 }
-
-// const Search = ({
-//   value,
-//   onChange,
-//   onSubmit,
-//   children
-// }) =>
-//   <form onSubmit={onSubmit}>
-//     <input
-//       type="text"
-//       value={value}
-//       onChange={onChange}
-//     />
-//     <button type="submit">
-//       {children}
-//     </button>
-//   </form>
 
 const Table = ({ list, onDismiss }) =>
   <div className="table">
